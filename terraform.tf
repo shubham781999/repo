2940923ec99aca0082aaa941fd99d094c049ba65
shubham781999 <138647879+shubham781999@users.noreply.github.com>
2024-09-09 16:28:10 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAUS4SSG6ZFHD67XKG"
-  secret_key = "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"
+  access_key = "AKIAUS4SSG6ZFJHD7XKG"
+  secret_key = "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"
 }
 
 variable "enable_public_ip" {
